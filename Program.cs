57f6300bb6a3a8599effd92f6f37acfa9d05be31
VitorHugoNP <supimpayey@gmail.com>
2024-08-28 10:46:06 -0300

    Feito o AlunosController na pasta controller

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
+using pdtcc_doc_academy.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDBContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
 var app = builder.Build();
 
