7c0d264f893a230cc500d21726533baa68568b40
VitorHugoNP <supimpayey@gmail.com>
2024-11-27 16:08:34 -0300

    Models agora apresentam Display para melhor aparência nas views

--- Models/Funcionarios.cs
+++ Models/Funcionarios.cs
@@ -8,22 +8,26 @@ namespace pdtcc_doc_academy.Models
     public class Funcionario
     {
         [Key]
+        [Display(Name = "Id do Funcionario")]
         public int idFuncionario { get; set; }
 
         [Required]
         [MaxLength(45)]
-        
+        [Display(Name = "Nome do Funcionario")]
         public string nome_func { get; set; }
 
         [Required]
         [MaxLength(150)]
+        [Display(Name = "Email do Funcionario")]
         public string email_func { get; set; }
 
         [Required]
         [MaxLength(150)]
+        [Display(Name = "Senha do Funcionario")]
         public string senha_func { get; set; }
         
         [ForeignKey("fk_escola")]
+        [Display(Name = "Id da Escola")]
         public int? fk_escola { get; set; }
         
         [ForeignKey("fk_usuario")]
